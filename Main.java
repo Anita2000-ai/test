b4d76d3208d2919e63ad23393282459dcc9217a6
Anita <57784998+Anita2000-ai@users.noreply.github.com>
2022-01-19 14:52:50 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package practise;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("learning can be great");
+
+	}
+
+}
